a227fe346ec758ee55335404b9eb004e8f00c81c
Francisco Bahamondes <francisco.bahamondes90@gmail.com>
2021-01-05 13:19:26 -0300

    remove navbar from views

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,14 +1,11 @@
 <template>
-  <Navbar />
   <Imagen texto = "The Northside Youth Soccer League aspires to develop strong, well-rounded, and mindful athletes through the building of character, self-discipline, and leadership."/>
 </template>
 <script>
-import Navbar from "@/components/Navbar.vue";
 import Imagen from "@/components/Imagen.vue";
 export default {
   name: "About",
   components: {
-    Navbar,
     Imagen
   },
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
-    <Navbar />
     <Card fecha = "4 Agosto" cuerpo= "NYSL Fundraiser" />
     <Card fecha = "16 Agosto" cuerpo= "Season Kick-off: Meet the Teams" />
     <Card fecha = "1 Septiembre" cuerpo= "First Game of the Season (Check Game Schedule for details)" />
   <router-view />
 </template>
-
 <script>
-import Navbar from "@/components/Navbar.vue";
 import Card from "@/components/Card.vue";
 export default {
   name: "Home",
   components: {
-    Navbar,
     Card
   },
 };
