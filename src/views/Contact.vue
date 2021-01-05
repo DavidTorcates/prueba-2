76dd86a318aef49d8555190b3a0fd91b37916add
Francisco Bahamondes <francisco.bahamondes90@gmail.com>
2021-01-05 14:57:13 -0300

    contact and form components

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,50 +1,16 @@
 <template>
   <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <p>¿Tienes alguna consulta sobre algún partido?</p>
-        <p>Escríbenos y te contactaremos a la brevedad</p>
-        <form action="#" method="post">
-          <div class="row">
-            <div class="col-4">
-              <label for="nombre">Nombre:</label>
-              
-            </div>
-            <div class="col-8">
-              <input type="text" name="nombre" id="nombre" />
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-4">
-              <label for="email">E-Mail:</label>
-            </div>
-            <div class="col-8">
-              <input type="text" name="email" id="email" />
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-4">
-              <label for="consulta">Consulta:</label>
-            </div>
-            <div class="col-8">
-              <input type="text" name="consulta" id="consulta" />
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-4">
-              <input type="submit" value="Enviar" />
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
+    <Form/>
   </div>
   <router-view />
 </template>
 
 <script>
+import Form from "@/components/Form.vue";
 export default {
   name: "Home",
-  components: {},
+  components: {
+    Form,
+  },
 };
 </script>
